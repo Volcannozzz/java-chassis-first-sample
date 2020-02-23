df7ea37e0bf21f8528f34b9e10ce111edaacf353
yhs0092 <yhs0092@163.com>
2020-02-23 20:16:33 +0800

    complete first sample

---
+++ src/main/java/org/apache/servicecomb/samples/AppMain.java
@@ -0,0 +1,9 @@
+package org.apache.servicecomb.samples;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+
+public class AppMain {
+  public static void main(String[] args) {
+    BeanUtils.init();
+  }
+}
